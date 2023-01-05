c4cd02a3d4b7d887533091ba90612bfc1a8ca566
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2023-01-04 22:44:34 -0300

    capitulo 6...

--- cap6/vetoresInicial.js
+++ cap6/vetoresInicial.js
@@ -32,3 +32,21 @@ console.log(letra);
 const letra4 = letra.splice(0, 2); // OS DOIS PRIMEIROS, NO (2) PARA
 console.log(letra); 
 console.log(letra4); //A B
+
+for (const cidade of cidades) {
+    console.log(cidade);
+  }
+  console.log("-".repeat(20));
+  
+  cidades.forEach((cidade, i) => {
+    console.log(`${i + 1}º Cidade: ${cidade}`);
+  });
+  
+  console.log("-".repeat(20));
+  //For each para somatório
+  const numeros = [5, 10, 15, 20];
+  let soma = 0;
+  
+  numeros.forEach(num => soma += num);
+  console.log(soma);
+  
