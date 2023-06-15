d7da6e7134c32844550a3f00899441ccb63e3710
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2023-06-14 23:51:31 -0300

    cap8 prática

--- cap8/script.js
+++ cap8/script.js
@@ -2,17 +2,31 @@ let ano = document.getElementById("ano");
 let modelo = document.getElementById("modelo");
 let preco = document.getElementById("preco");
 
+let modeloCarro = document.getElementById("modeloCarro");
 let entradaCarro = document.getElementById("entradaCarro");
 let parcelasCarro = document.getElementById("parcelasCarro");
+let btnCalcular = document.getElementById("btnCalcular");
 
+btnCalcular.addEventListener("click", e => {
+  ano = ano.value;
+  preco = preco.value;
 
-let classificarCarro = (anoCarro, anoAtual = 2022) => {
+  
+  const situacao = classificarCarro(ano);
+  const entrada = valorEntrada(preco);
+
+  modeloCarro.textContent = modelo.value + " - " + situacao;
+  entradaCarro.textContent = "Entrada: R$" + entrada;
+  parcelasCarro.textContent = situacao;
+})
+
+let classificarCarro = (ano, anoAtual = 2023) => {
   let classificando = "";
-  if (anoCarro == anoAtual) {
+  if (ano == anoAtual) {
     classificando = "Novo";
-  } if (anoCarro <= anoAtual - 1) {
+  } if (ano <= anoAtual - 1) {
     classificando = "Semi-Novo";
-  } if (anoCarro <= 2020) {
+  } if (ano <= 2020) {
     classificando = "Usado";
   }
   return classificando;
@@ -21,7 +35,7 @@ let classificarCarro = (anoCarro, anoAtual = 2022) => {
 let valorEntrada = (precoCarro) => {
   let entrada;
   let preco2 = preco.value;
- 
+
   if (classificarCarro(ano.value) == "Novo") {
     entrada = preco2 * 0.5;
   } else {
@@ -36,29 +50,12 @@ let valorParcelas = (precoCarro) => {
   if (classificarCarro(ano.value) == "Novo") {
     parcelasDe = valorEntrada(preco.value) / 10;
   } else {
-    parcelasDe = (preco2 - valorEntrada(preco.value)) /10 ;
+    parcelasDe = (preco2 - valorEntrada(preco.value)) / 10;
   }
 
   return parcelasDe;
 }
 
-const situacao = classificarCarro(ano);
 
 
-function procedimentos() {
-  let ano2 = ano.value;
-  let modeloCarro2 = modelo.value;
 
-  let situacao = classificarCarro(ano2);
-  modeloCarro.textContent = `${modeloCarro2} - ${situacao} `;
-  let preco2 = preco.value;
-  let entrada2 = valorEntrada(preco2);
-
-  entradaCarro.textContent = `Valor de entrada: R$ ${entrada2}`;
-  
-  
-  let parcelas = valorParcelas(preco.value);
-
-  parcelasCarro.textContent = `10x de de: R$ ${parcelas}`;
-
-}
