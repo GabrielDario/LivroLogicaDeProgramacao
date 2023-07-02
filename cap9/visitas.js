5535713ecfd428ea529809ab983032e442512c69
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2023-07-01 22:49:48 -0300

    Criando primeiro grupo para copa do mundo - LISTAS E MANIPULAÇÕES

---
+++ cap9/visitas.js
@@ -0,0 +1,19 @@
+
+
+let visitas = localStorage.getItem("visitas");
+
+console.log(typeof visitas)
+if(visitas == 0) {
+  alert('Bem vindo pela primeira vez no nosso site!');
+  visitas = Number(visitas);
+  visitas ++;
+  visitas = visitas.toString();
+  localStorage.setItem("visitas", visitas);
+} else {
+  console.log(visitas)
+  alert('Bem vindo,essa é a ' +  visitas + 'º vez que voce visita' );
+  visitas = Number(visitas);
+  visitas++;
+  visitas = visitas.toString();
+  localStorage.setItem("visitas", visitas);
+}
